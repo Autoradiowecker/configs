24539b7804c49b27d4b1b8c94f5213a498c6ea13
Autoradiowecker <clemens-g@gmx.de>
2024-02-17 20:15:46 +0100

    test

--- configuration.nix
+++ configuration.nix
@@ -216,6 +216,7 @@ in
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+#test
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
